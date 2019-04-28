eeba382993e75ff418a3636f63a4f9dcae162873
huangjie <15907081130@163.com>
2019-04-28 12:13:35 +0800

    'init'

---
+++ src/main/java/com/hj/App.java
@@ -0,0 +1,14 @@
+/*
+ * JLuster.com Inc.
+ * Copyright (c) 2019-2019 All Rights Reserved.
+ */
+
+package com.hj;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/com/hj/AppTest.java
@@ -0,0 +1,17 @@
+/*
+ * JLuster.com Inc.
+ * Copyright (c) 2019-2019 All Rights Reserved.
+ */
+
+package com.hj;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void test001() {
+
+    }
+
+}
